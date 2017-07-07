37e62f6e36632da7012b2299d8cecd8243792932
benneely <nigelneely@gmail.com>
2017-07-07 09:04:29 -0400

    fixes issue #2, 502 caused by gunicorn worker terminating early, added timeout flag to increase worker time

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
-gunicorn --bind unix:/mmci-practical-datascience-competition/competition.sock competition.wsgi:application &
+gunicorn --timeout 300 --bind unix:/mmci-practical-datascience-competition/competition.sock competition.wsgi:application &
 nginx -g "daemon off;"
